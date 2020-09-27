3265c8a7565aa1549c74771465e7c122d4974ed1
uapgp <uapgp88@gmail.com>
2020-09-27 18:22:23 +0200

    Web upload

---
+++ sketch.js
@@ -0,0 +1,593 @@
+
+
+//Moviment c1
+let xmv = 920;
+let ymv = 70;
+let bolet = false
+
+
+//Colorss i Alpha
+let redC = 0;
+let alph = 0;
+
+function Ci1_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+   
+    
+    if(xmv <= 600) bolet = true;
+    //if(xmv >= 900) bolet = false;
+
+    if (bolet == false){
+      xmv -= dif1;
+      ymv += dif1;
+
+    }
+
+    if (bolet == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv);
+    console.log(bolet);
+
+    circle(xmv, ymv, 80 );
+
+}
+
+
+//Moviment c2
+let xmv2 = 600;
+let ymv2 = 500;
+let bolet2 = false
+
+
+
+
+function Ci2_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    //if(xmv2 <= 600) bolet2 = false;
+    if(xmv2 >= 950) bolet2 = true;
+
+    if (bolet2 == false){
+      xmv2 += dif1;
+      ymv2 -= dif1;
+
+    }
+
+    if (bolet2 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv2);
+    console.log(bolet2);
+
+    circle(xmv2, ymv2, 80 );
+
+}
+
+
+//Moviment c3
+let xmv3 = 1205;
+let ymv3 = 10;
+let bolet3 = false
+
+
+
+
+function Ci3_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    if(xmv3 <= 1000) bolet3 = true;
+    //if(xmv3 >= 970) bolet3 = true;
+
+    if (bolet3 == false){
+      xmv3 -= dif1;
+      ymv3 += dif1;
+
+    }
+
+    if (bolet3 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv3);
+    console.log(bolet3);
+
+    circle(xmv3, ymv3,80);
+
+}
+
+
+
+//Moviment c4
+let xmv4 = 950;
+let ymv4 = 265;
+let bolet4 = false
+
+
+
+
+function Ci4_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    if(xmv4 <= 700) bolet4 = true;
+    //if(xmv3 >= 970) bolet3 = true;
+
+    if (bolet4 == false){
+      xmv4 -= dif1;
+      ymv4 += dif1;
+
+    }
+
+    if (bolet4 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv4);
+    console.log(bolet4);
+
+    circle(xmv4, ymv4,80);
+
+}
+
+
+//Moviment c5
+let xmv5 = 550;
+let ymv5 = 780;
+let bolet5 = false
+
+
+
+
+function Ci5_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    //if(xmv5 <= 700) bolet5 = false;
+    //if(xmv3 >= 970) bolet3 = true;
+    //if(xmv2 <= 600) bolet2 = false;
+    if(xmv5 >= 970) bolet5 = true;
+
+    if (bolet5 == false){
+      xmv5 += dif1;
+      ymv5 -= dif1;
+
+    }
+
+    if (bolet5 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv5);
+    console.log(bolet5);
+
+    circle(xmv5, ymv5,80);
+
+}
+
+
+
+//Moviment c6
+let xmv6 = 950;
+let ymv6 = 495;
+let bolet6 = false
+
+
+
+
+function Ci6_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    if(xmv6 <= 700) bolet6 = true;
+    //if(xmv3 >= 970) bolet3 = true;
+    //if(xmv2 <= 600) bolet2 = false;
+    //if(xmv6 >= 970) bolet6 = true;
+
+    if (bolet6 == false){
+      xmv6 -= dif1;
+      ymv6 += dif1;
+
+    }
+
+    if (bolet6 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv6);
+    console.log(bolet6);
+
+    circle(xmv6, ymv6,80);
+
+}
+
+
+//Moviment c7
+let xmv7 = 1205;
+let ymv7 = 250;
+let bolet7 = false
+
+
+function Ci7_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    if(xmv7 <= 1030) bolet7 = true;
+    //if(xmv3 >= 970) bolet3 = true;
+
+    if (bolet7 == false){
+      xmv7 -= dif1;
+      ymv7 += dif1;
+
+    }
+
+    if (bolet7 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv7);
+    console.log(bolet7);
+
+    circle(xmv7, ymv7,80);
+
+}
+
+//Moviment c8
+let xmv8 = 550;
+let ymv8 = 1010;
+let bolet8 = false
+
+
+
+
+function Ci8_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    //if(xmv5 <= 700) bolet5 = false;
+    //if(xmv3 >= 970) bolet3 = true;
+    //if(xmv2 <= 600) bolet2 = false;
+    if(xmv8 >= 970) bolet8 = true;
+
+    if (bolet8 == false){
+      xmv8 += dif1;
+      ymv8 -= dif1;
+
+    }
+
+    if (bolet8 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv8);
+    console.log(bolet8);
+
+    circle(xmv8, ymv8,80);
+
+}
+
+//Moviment c9
+let xmv9 = 950;
+let ymv9 = 725;
+let bolet9 = false
+
+
+
+
+function Ci9_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    if(xmv9 <= 700) bolet9 = true;
+    //if(xmv3 >= 970) bolet3 = true;
+    //if(xmv2 <= 600) bolet2 = false;
+    //if(xmv6 >= 970) bolet6 = true;
+
+    if (bolet9 == false){
+      xmv9 -= dif1;
+      ymv9 += dif1;
+
+    }
+
+    if (bolet9 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv9);
+    console.log(bolet9);
+
+    circle(xmv9, ymv9,80);
+
+}
+
+
+//Moviment c10
+let xmv10 = 1205;
+let ymv10 = 450;
+let bolet10 = false
+
+
+function Ci10_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    if(xmv10 <= 1030) bolet10 = true;
+    //if(xmv3 >= 970) bolet3 = true;
+
+    if (bolet10 == false){
+      xmv10 -= dif1;
+      ymv10 += dif1;
+
+    }
+
+    if (bolet10 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv10);
+    console.log(bolet10);
+
+    circle(xmv10, ymv10,80);
+
+}
+
+
+//Moviment c11
+let xmv11 = 550;
+let ymv11 = 1240;
+let bolet11 = false
+
+
+
+
+function Ci11_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    //if(xmv5 <= 700) bolet5 = false;
+    //if(xmv3 >= 970) bolet3 = true;
+    //if(xmv2 <= 600) bolet2 = false;
+    if(xmv11 >= 970) bolet11 = true;
+
+    if (bolet11 == false){
+      xmv11 += dif1;
+      ymv11 -= dif1;
+
+    }
+
+    if (bolet11 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv11);
+    console.log(bolet11);
+
+    circle(xmv11, ymv11,80);
+
+}
+
+
+
+//Moviment c12
+let xmv12 = 950;
+let ymv12 = 955;
+let bolet12 = false
+
+
+
+
+function Ci12_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    if(xmv12 <= 700) bolet12 = true;
+    //if(xmv3 >= 970) bolet3 = true;
+    //if(xmv2 <= 600) bolet2 = false;
+    //if(xmv6 >= 970) bolet6 = true;
+
+    if (bolet12 == false){
+      xmv12 -= dif1;
+      ymv12 += dif1;
+
+    }
+
+    if (bolet12 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv12);
+    console.log(bolet12);
+
+    circle(xmv12, ymv12,80);
+
+}
+
+//Moviment c13
+let xmv13 = 1205;
+let ymv13 = 650;
+let bolet13 = false
+
+
+function Ci13_Movement(){
+    
+  fill(redC,0,0,0.5);
+  
+  if (redC < 255) {
+    redC = redC+0.13;
+  }
+  
+    let dif1 = 0.18;
+    let dif2 = 0.2
+    
+    if(xmv13 <= 1030) bolet13 = true;
+    //if(xmv3 >= 970) bolet3 = true;
+
+    if (bolet13 == false){
+      xmv13 -= dif1;
+      ymv13 += dif1;
+
+    }
+
+    if (bolet13 == true){
+     // xmv -= dif1
+     // ymv -= dif1
+      
+    }
+    console.log(xmv13);
+    console.log(bolet13);
+
+    circle(xmv13, ymv13,80);
+
+}
+//Setup
+function setup() {
+
+  let dW = 1920;
+  let dH = 1080;
+
+  
+     
+  createCanvas(dW, dH);
+
+    
+  background(0);
+  
+  noStroke()
+
+ 
+}
+
+function draw() {
+  textSize(75);
+
+
+   
+  Ci1_Movement();
+  Ci2_Movement();
+  Ci3_Movement();
+  Ci4_Movement();
+  Ci5_Movement();
+  Ci6_Movement();
+  Ci7_Movement();
+  Ci8_Movement();
+  Ci9_Movement();
+  Ci10_Movement();
+  Ci11_Movement();
+  Ci12_Movement();
+  Ci13_Movement();
+
+
+  fill(255, 255, 255);
+  text('R', 1980/2.1, 100);
+  text('É', 1980/1.9, 200);
+  text('S', 1980/2.1, 300);
+  text('O', 1980/1.9, 400);
+  text('N', 1980/2.1, 500);
+  text('A', 1980/1.9, 600);
+  text('N', 1980/2.1, 720);
+  text('C', 1980/1.9, 820);
+  text('E', 1980/2.1, 950);
+
+  
+}
+
